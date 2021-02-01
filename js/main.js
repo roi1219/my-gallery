fb340b181b4985f26670eb967a33fc13bdeb8b34
Roi Levy <levy6161@gmail.com>
2021-02-01 16:42:44 +0200

    render projects and modals

--- js/main.js
+++ js/main.js
@@ -1,1 +1,55 @@
+'use strict'
+
 console.log('Starting up');
+
+$(document).ready(init);
+
+function init() {
+    renderProjs();
+}
+
+function renderProjs() {
+    var projs = getProjs();
+    var strHTML = projs.map(function (proj) {
+        return `
+        <div onclick="renderModal('${proj.id}')" class="col-md-4 col-sm-6 portfolio-item">
+          <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1">
+            <div class="portfolio-hover">
+              <div class="portfolio-hover-content">
+                <i class="fa fa-plus fa-3x"></i>
+              </div>
+            </div>
+            <img class="img-fluid" src="${proj.imgURL}" alt="">
+          </a>
+          <div class="portfolio-caption">
+            <h4>${proj.name}</h4>
+            <p class="text-muted">${proj.title}</p>
+          </div>
+        </div>`
+    }).join('');
+    $('.proj-container').html(strHTML);
+}
+
+function renderModal(projID) {
+    console.log('hi');
+    var proj = getProjById(projID);
+    var date=new Date(proj.publishedAt);
+    console.log('date:', proj.publishedAt)
+    var strHTML = `<h2>${proj.name}</h2>
+    <p class="item-intro text-muted">${proj.title}.</p>
+    <img class="img-fluid d-block mx-auto" src="${proj.imgURL}" alt="">
+    <p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est
+      blanditiis
+      dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae
+      cupiditate,
+      maiores repudiandae, nostrum, reiciendis facere nemo!</p>
+      <ul class="list-inline">
+        <li>Date: ${date}</li>
+      <li>Client: Explore</li>
+      <li>Category: Graphic Design</li>
+    </ul>
+    <button class="btn btn-primary" data-dismiss="modal" type="button">
+      <i class="fa fa-times"></i>
+      Close Project</button>`;
+      $('.proj-modal').html(strHTML);
+}
---
+++ js/proj-services.js
@@ -0,0 +1,44 @@
+'use strict'
+
+var gProjs = [
+    {
+        id: 'minesweeper',
+        name: 'Minesweeper',
+        title: 'game',
+        desc: 'logic game',
+        url: 'projs\Minesweeper',
+        imgURL: 'img/portfolio/minesweeper.png',
+        publishedAt: '1611433841000'
+    },
+    {
+        id: 'bookShop',
+        name: 'Book Shop',
+        title: 'manage your books',
+        desc: 'a comptrable place to manage your book store.',
+        url: 'projs\BookShop',
+        imgURL: 'img/portfolio/bookshop.png',
+        publishedAt: '1612038641000'
+    },
+    {
+        id: 'pacman',
+        name: 'Pacman',
+        title: 'The nostalgic game in a new inviorment',
+        desc: 'arcade game',
+        url: `projs\pacman`,
+        imgURL: 'img/portfolio/pacman.png',
+        publishedAt: '1611001841000'
+    },
+];
+
+
+function getProjs(){
+    return gProjs;
+}
+
+function getProjById(projID){
+    return gProjs.find(function(proj){
+        return proj.id===projID;
+    })
+}
+
+{/* <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1"> */}
